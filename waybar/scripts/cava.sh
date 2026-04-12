d43d6a236a4270559e1ca5c9598d0a97a112fdfb
yoyosmu <jun.mu57@gmail.com>
2026-04-12 14:59:09 -0400

    Cool Stuff

--- waybar/scripts/cava.sh
+++ waybar/scripts/cava.sh
@@ -8,10 +8,10 @@ cava -p ~/.config/cava/waybar | while IFS=';' read -ra vals; do
             1) chars="${chars}▂" ;;
             2) chars="${chars}▃" ;;
             3) chars="${chars}▄" ;;
-            4) chars="${chars}▅" ;;
-            5) chars="${chars}▆" ;;
-            6) chars="${chars}▇" ;;
-            7) chars="${chars}█" ;;
+            4) chars="${chars}▄" ;;
+            5) chars="${chars}▅" ;;
+            6) chars="${chars}▆" ;;
+            7) chars="${chars}▇" ;;
             8) chars="${chars}█" ;;
 
         esac
