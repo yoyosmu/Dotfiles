69b283b94cd94b5c9bbe64d949f6909e5deeed7b
yoyosmu <jun.mu57@gmail.com>
2026-07-10 15:49:45 -0400

    Cool Stuff

---
+++ spicetify/Themes/Hazy/theme.js
@@ -0,0 +1,9 @@
+(() => {
+    const script = document.createElement("SCRIPT");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute(
+      "src",
+      "https://cdn.jsdelivr.net/gh/astromations/hazy/hazy.js"
+    );
+    document.head.appendChild(script);
+  })();
